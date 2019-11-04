cb1a5e540097daefa99296855d1642e8e701b012
Mikhael Dionio <mikhaeldionio@gmail.com>
2019-11-04 13:40:29 +0800

    first commit

---
+++ public/js/game.js
@@ -0,0 +1,442 @@
+//Classes
+class Projectile{
+    constructor(projectileType, imageReference){
+        this.type = projectileType;
+        
+        if(this.type == "stone"){
+            this.speed = 0;
+        }else{
+            this.speed = 30;
+        }
+
+        this.image = imageReference;
+    }
+
+    getType(){
+        return this.type;
+    }
+
+    getSpeed(){
+        return this.speed;
+    }
+
+    setSpeed(value){
+        this.speed = value;
+    }
+}
+
+class Player{
+    constructor(playerID,roomId){
+        this.playerID = playerID;
+        this.roomId = roomId;
+        this.currLane = 0;
+        this.health = 100;
+    }
+
+    getHealth(){
+        return this.health;
+    }
+
+    takeDamage(damageAmount){
+        this.health = this.health - damageAmount;
+        if(this.health < 0){
+            this.health = 0;
+        }
+        return this.health;
+    }
+}
+
+//Gestures
+var gest = new gestures({
+	debug: true,
+	draw: true,
+	drawColor: "#000000",
+	drawWidth: 5,
+	autoTrack: true,
+	allowRotation: true,
+	inverseShape: true,
+	points: 33
+});
+
+gest.addGesture("Line", [
+	{x: 0, y: 0},
+	{x: 0, y: 25},
+	{x: 0, y: 50},
+	{x: 0, y: 75},
+	{x: 0, y: 100}
+], throwProjectile);
+
+gest.addGesture("Square", [
+	{x: 0, y: 0},
+	{x: 100, y: 0},
+	{x: 200, y: 0},
+	{x: 200, y: 100},
+	{x: 200, y: 200},
+	{x: 100, y: 200},
+	{x: 0, y: 200},
+	{x: 0, y: 100},
+	{x: 0, y: 0}
+], throwProjectile);
+
+gest.addGesture("Rectangle", [
+	{x: 0, y: 0},
+	{x: 105, y: 0},
+	{x: 210, y: 0},
+	{x: 210, y: 50},
+	{x: 210, y: 100},
+	{x: 105, y: 100},
+	{x: 0, y: 100},
+	{x: 0, y: 50},
+	{x: 0, y: 0}
+], throwProjectile);
+
+gest.addGesture("ZigZag", [
+	{x: 0, y: 0},
+	{x: 25, y: 44},
+	{x: 50, y: 87},
+	{x: 70, y: 44},
+	{x: 100, y: 0},
+	{x: 125, y: 44},
+	{x: 150, y: 87}
+], throwProjectile);
+
+gest.addGesture("Triangle", [
+	{x: 0, y: 0},
+	{x: 50, y: 50},
+	{x: 100, y: 100},
+	{x: 50, y: 100},
+	{x: 0, y: 100},
+	{x: 0, y: 50},
+	{x: 0, y: 0}
+], throwProjectile);
+
+gest.addGesture("Equilateral Triangle", [
+	{x: 0, y: 0},
+	{x: 25, y: 44},
+	{x: 50, y: 87},
+	{x: 75, y: 44},
+	{x: 100, y: 0},
+	{x: 50, y: 0},
+	{x: 0, y: 0}
+], throwProjectile);
+
+gest.addGesture("Check", [
+	{x: 0, y: 0},
+	{x: 25, y: 25},
+	{x: 50, y: 50},
+	{x: 75, y: 25},
+	{x: 100, y: 0},
+], throwProjectile);
+
+var x = 0;
+var y = -100;
+var circle = [];
+var totalPoints = 72;
+var step = (Math.PI*2)/totalPoints;
+
+for(var angle = 1; angle < totalPoints; angle++)
+{
+	var newX = x*Math.cos(angle*step)-y*Math.sin(angle*step);
+	var newY = y*Math.cos(angle*step)+x*Math.sin(angle*step);
+	var point = {x: newX, y: newY};
+	circle.push(point);
+}
+
+gest.addGesture("Circle", circle, throwProjectile);
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 400,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: false,
+        gravity: { y: 0 }
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    } 
+};
+   
+var game = new Phaser.Game(config);
+var emitter = new Phaser.Events.EventEmitter();
+var otherPlayer;
+var player;
+var startGame = true;
+
+function preload() {
+    this.load.image('ship','assets/spaceShips_001.png');
+    this.load.image('otherPlayer','assets/enemyBlack5.png');
+    this.load.image('fire','assets/star_full.png');
+    this.load.image('nature','assets/triangle.png');
+    this.load.image('wind','assets/wind.png');
+    this.load.image('water','assets/water.png');
+    this.load.image('lightning','assets/lightning.png');
+    this.load.image('stone','assets/stone.png')
+    this.load.image('health_bar','assets/bar.png');
+    this.load.image('bg','assets/bg.png');
+}
+   
+function create() {
+    var self = this;
+    this.socket = io();
+    this.otherPlayers = this.physics.add.group();
+    
+
+    //Background
+    this.background = this.add.image(200,200,'bg').setOrigin(0.5,0.5);
+    //Projectiles
+    this.myProjectiles = this.physics.add.group();//Projectiles sent by me on the field
+    this.otherProjectiles = this.physics.add.group();//Projectiles sent by opponent on the field
+
+    this.physics.add.overlap(this.myProjectiles.getChildren(), this.otherProjectiles.getChildren(), function(myProjectile, otherProjectile){
+        if(myProjectile.weaknesses.includes(otherProjectile.type)){
+            self.myProjectiles.remove(myProjectile);
+            myProjectile.destroy();
+        }
+
+        if(otherProjectile.weaknesses.includes(myProjectile.type)){
+            self.otherProjectiles.remove(otherProjectile);
+            otherProjectile.destroy();
+        }
+    });
+
+    // console.log(this.myProjectiles);
+    // console.log(this.otherProjectiles);
+    //Handle projectile collisions
+    // console.log(this.physics.add.collider(this.myProjectiles, this.otherProjectiles, function(myProjectile, otherProjectile){
+    //     console.log("collision");
+    // }));
+
+    this.socket.on('currentPlayers', function(players) {
+        Object.keys(players).forEach(function (id) {
+            if (players[id].playerId === self.socket.id) {
+                addPlayer(self, players[id]);
+            } else {
+                addOtherPlayers(self, players[id]);
+            }
+        });
+    });
+
+    this.socket.on('newPlayer',function(playerInfo){
+        addOtherPlayers(self, playerInfo);
+    });
+
+    this.socket.on('disconnect', function(playerId){
+        self.otherPlayers.getChildren().forEach(function (otherPlayer){
+            if(playerId === otherPlayer.playerId){
+                otherPlayer.destroy();
+            }
+        });
+    });
+
+    //Player input
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    //Change Lane
+    this.input.on('pointerdown', function(pointer){
+        console.log(pointer.x + ", " + pointer.y);
+        if(self.player){
+            if(pointer.x < 128){
+                self.player.currLane = 0;
+            }else if(pointer.x < 271){
+                self.player.currLane = 1;
+            }else{
+                self.player.currLane = 2;
+            }
+            console.log(self.player.currLane);
+        }
+    }, this);
+
+    //Throw Projectile
+    // this.input.keyboard.on('keydown_Q', function(event){//Fire
+    //     const pointer = self.input.activePointer;
+    //     self.myProjectiles.add(addProjectile(self, "fire",laneToCoord(self.player.currLane), 380));
+    //     this.socket.emit('playerInput', {type: "fire", x: laneToCoord(self.player.currLane), y: 380, roomId: self.player.roomId});
+    // }, this);
+
+    // this.input.keyboard.on('keydown_E', function(event){//Nature
+    //     const pointer = self.input.activePointer;
+    //     self.myProjectiles.add(addProjectile(self, "nature",laneToCoord(self.player.currLane), 380));
+    //     this.socket.emit('playerInput', {type: "nature", x: laneToCoord(self.player.currLane), y: 380, roomId: self.player.roomId});
+    // }, this);
+
+    // this.input.keyboard.on('keydown_W', function(event){//Water
+    //     const pointer = self.input.activePointer;
+    //     self.myProjectiles.add(addProjectile(self, "water",laneToCoord(self.player.currLane), 380));
+    //     this.socket.emit('playerInput', {type: "water", x: laneToCoord(self.player.currLane), y: 380, roomId: self.player.roomId});
+    // }, this);
+
+    // this.input.keyboard.on('keydown_A', function(event){//Lightning
+    //     const pointer = self.input.activePointer;
+    //     self.myProjectiles.add(addProjectile(self, "lightning",laneToCoord(self.player.currLane), 380));
+    //     this.socket.emit('playerInput', {type: "lightning", x: laneToCoord(self.player.currLane), y: 380, roomId: self.player.roomId});
+    // }, this);
+
+    // this.input.keyboard.on('keydown_D', function(event){//Wind
+    //     const pointer = self.input.activePointer;
+    //     self.myProjectiles.add(addProjectile(self, "wind",laneToCoord(self.player.currLane), 380));
+    //     this.socket.emit('playerInput', {type: "wind", x: laneToCoord(self.player.currLane), y: 380, roomId: self.player.roomId});
+    // }, this);
+
+    // this.input.keyboard.on('keydown_S', function(event){//Stone
+    //     const pointer = self.input.activePointer;
+    //     self.myProjectiles.add(addProjectile(self, "stone",laneToCoord(self.player.currLane), 380));
+    //     this.socket.emit('playerInput', {type: "stone", x: laneToCoord(self.player.currLane), y: 380, roomId: self.player.roomId});
+    // }, this);
+
+    emitter.on('throw_projectile', function(type){
+        self.myProjectiles.add(addProjectile(self, type,laneToCoord(self.player.currLane), 380));
+        this.socket.emit('playerInput', {type: type, x: laneToCoord(self.player.currLane), y: 380, roomId: self.player.roomId});
+    }, this)
+
+    this.socket.on('playerClicked',function(projectileData){
+        //const op = self.physics.add.image(projectileData.x,600 - projectileData.y,'otherPlayer');//relative to screen height
+        //op.setVelocityY(3);
+        self.otherProjectiles.add(addProjectile(self,projectileData.type,projectileData.x, 40));
+        console.log(self.otherProjectiles);
+    }, this);
+}
+
+function update() {
+    var self = this;
+    
+    if(this.ship){
+        if(this.cursors.left.isDown){
+            this.ship.setAngularVelocity(-150);
+        }else if (this.cursors.right.isDown){
+            this.ship.setAngularVelocity(150);
+        } else {
+            this.ship.setAngularVelocity(0);
+        }
+
+        if (this.cursors.up.isDown){
+            this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+        } else {
+            this.ship.setAcceleration(0);
+        }
+    }
+
+    this.myProjectiles.getChildren().forEach(function(projectileObject) {//Behaviour of projectiles sent by me
+        projectileObject.setVelocityY(-projectileObject.speed);
+        //projectileObject.body.debugBodyColor = projectileObject.body.touching.none ? 0x0099ff : 0xff9900;
+        if(projectileObject.y < 40){
+            if(self.otherPlayer){
+                self.otherPlayer.takeDamage(10);
+                self.otherPlayer.healthBar.displayWidth = 400*(self.otherPlayer.getHealth()/100);
+            }
+            self.myProjectiles.remove(projectileObject);//stop tracking projectile
+            projectileObject.destroy();
+        }
+    });
+
+    this.otherProjectiles.getChildren().forEach(function(projectileObject) {//Behaviour of projectiles sent by other user
+        projectileObject.setVelocityY(projectileObject.speed);
+        if(projectileObject.y > 380){
+            if(self.player){
+                self.player.takeDamage(10);
+                self.player.healthBar.displayWidth = 400*(self.player.getHealth()/100);
+            }
+            self.otherProjectiles.remove(projectileObject);//stop tracking projectile
+            projectileObject.destroy();
+        }
+    });
+}
+
+function addPlayer(self, playerInfo){
+    self.player = new Player(playerInfo.playerId,playerInfo.roomId);//temp values
+    self.player.healthBar = self.add.sprite(200,400,'health_bar').setOrigin(0.5,0.5).setDisplaySize(400*(self.player.health/100),40);
+}
+
+function addOtherPlayers(self, playerInfo) {
+    //const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5,0.5).setDisplaySize(53,40);
+    self.otherPlayer = new Player(playerInfo.name,playerInfo.sessionId)
+    self.otherPlayer.healthBar = self.add.sprite(200,10,'health_bar').setOrigin(0.5,0.5).setDisplaySize(400*(self.otherPlayer.health/100),40);
+    self.otherPlayer.healthBar.setTint(0x00ff00);
+}
+
+function addProjectile(self, projectileType, posx, posy){
+    const p = self.physics.add.image(posx,posy,projectileType);
+    p.type = projectileType;
+    if(projectileType == "stone"){
+        p.speed = 0;
+    }else{
+        p.speed = 30;
+    }
+
+    //Weaknesses
+    switch(projectileType){
+        case 'fire':
+            p.weaknesses = [p.type,"water","wind","stone"];
+            break;
+        case 'water':
+            p.weaknesses = [p.type,"lightning","nature","stone"];
+            break;
+        case 'wind':
+            p.weaknesses = [p.type,"water","nature","stone"];
+            break;
+        case 'lightning':
+            p.weaknesses = [p.type,"fire","wind","stone"];
+            break;
+        case 'nature':
+            p.weaknesses = [p.type,"fire","lightning","stone"];
+            break;
+        case 'stone':
+            p.weaknesses = [p.type,"fire","lightning","wind","water","nature"];
+            break;
+    }
+    //const projectile = new Projectile(projectileType, p);
+    //self.myProjectiles.add(p);
+    //console.log(projectile.speed);
+    return p;
+}
+
+function throwProjectile(name){
+    // var self = this;
+    var type = "";
+    console.log(name);
+    switch(name){
+        case 'Line':
+            type = "wind";
+            break;
+        case 'Rectangle':
+            type = "stone";
+            break;
+        case 'ZigZag':
+            type="lightning";
+            break;
+        case 'Triangle':
+            type="nature";
+            break;
+        case 'Check':
+            type="fire";
+            break;
+    }
+    console.log(type);
+    //const pointer = self.input.activePointer;
+    //Emit projectile event
+    emitter.emit('throw_projectile', type);
+}
+
+function laneToCoord(lane){
+    switch(lane){
+        case 0:
+            return 64;
+        case 1:
+            return 200;
+        case 2:
+            return 335;
+    }
+}
+
+//Gestures
+// function callback(name)
+// {
+// 	// count++;
+// 	// document.getElementById("result").innerHTML = "Result: " + name;
+// }
---
+++ server.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+var players = {};
+var roomCount = 0;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', function(socket){
+    // console.log('a user connected');
+
+    if(roomCount == 0){
+        socket.join('room'+roomCount);
+        newPlayer(socket, roomCount);
+        console.log(socket.id + ' has joined session ' + roomCount);
+        roomCount++;
+    }else{
+        for(i = 0; i < roomCount; i++){
+            if(!io.sockets.adapter.rooms['room'+i]){//If room does not exist, fill in the gap
+                socket.join('room'+i);
+                newPlayer(socket, i);
+                console.log(socket.id + ' has joined session ' + i);
+                break;
+            } else if(io.sockets.adapter.rooms['room'+i].length < 2){//If found room with 1 player, add player
+                socket.join('room'+i);
+                newPlayer(socket, i);
+                console.log(socket.id + ' has joined session ' + i);
+                break;
+            } else if(i == roomCount - 1){//If found no room with 1 player, create new room
+                socket.join('room'+roomCount);
+                newPlayer(socket, roomCount);
+                console.log(socket.id + ' has joined session ' + roomCount);
+                roomCount++;
+                break;
+            }
+        }
+
+    }
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+        //remove this player from our players object
+        delete players[socket.id];
+
+        //emit a message to all players to remove this player
+        io.emit('disconnect',socket.id);
+    });
+
+    socket.on('playerInput', function(projectileData){
+        //console.log(projectileData.roomId);
+        socket.broadcast.to(projectileData.roomId).emit('playerClicked',projectileData);
+    });
+
+});
+
+server.listen(8081, function() {
+    console.log(`Listening on ${server.address().port}`);
+});
+
+function newPlayer(socket, roomNo){
+    //create a new player and add it to our players object
+    players[socket.id] = {
+        playerId: socket.id,
+        roomId: 'room' + roomNo,
+    };
+
+    //send the players object to the new player
+    socket.emit('currentPlayers', players);
+
+    //update all other players of the new player
+    socket.broadcast.emit('newPlayer',players[socket.id]);
+}
