aba893aaa54cddec761b3ade462e95f55ef9f05b
Mikhael Dionio <mikhaeldionio@gmail.com>
2019-11-18 17:55:36 +0800

    improve matchmaking to prioritize users who have waited longer

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var players = {};
 var roomCount = 0;
+var nextRoom = 0;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,32 +15,67 @@ app.get('/', function (req, res){
 io.on('connection', function(socket){//when a new player connects
 
     if(roomCount == 0){//if no rooms, make new room
+        console.log("No rooms");
         socket.join('room'+roomCount);
         newPlayer(socket, roomCount);
         console.log(socket.id + ' has joined session ' + roomCount);
         roomCount++;
     }else{
-        for(i = 0; i < roomCount; i++){
-            if(!io.sockets.adapter.rooms['room'+i]){//If room does not exist, fill in the gap
-                socket.join('room'+i);
-                newPlayer(socket, i);
-                console.log(socket.id + ' has joined session ' + i);
-                break;
-            } else if(io.sockets.adapter.rooms['room'+i].length < 2){//If found room with 1 player, add player and send waiting player data to new player
-                socket.join('room'+i);
-                newPlayer(socket, i);
-                console.log(socket.id + ' has joined session ' + i);
-                break;
-            } else if(i == roomCount - 1){//If found no room with 1 player, create new room
-                socket.join('room'+roomCount);
-                newPlayer(socket, roomCount);
-                console.log(socket.id + ' has joined session ' + roomCount);
-                roomCount++;
-                break;
+        // for(i = 0; i < roomCount; i++){
+        //     if(!io.sockets.adapter.rooms['room'+i]){//If room does not exist, fill in gap
+        //         socket.join('room'+i);
+        //         newPlayer(socket, i);
+        //         console.log(socket.id + ' has joined session ' + i);
+        //         break;
+        //         // console.log("Room " + i + " does not exist");
+        //     } else if(io.sockets.adapter.rooms['room'+i].length < 2){//If found room with 1 player, add player and send waiting player data to new player
+        //         console.log("Old Room");
+        //         socket.join('room'+i);
+        //         newPlayer(socket, i);
+        //         console.log(socket.id + ' has joined session ' + i);
+        //         roomCount++;
+        //         break;
+        //     } else if(i == roomCount - 1){//If found no room with 1 player, create new room
+        //         console.log("New Room");
+        //         socket.join('room'+roomCount);
+        //         newPlayer(socket, roomCount);
+        //         console.log(socket.id + ' has joined session ' + roomCount);
+        //         // roomCount++;
+        //         break;
+        //     }
+        // }
+        var foundRoom = false;
+        for(i = 0; i < roomCount; i++){//Look for room with waiting user
+            if(io.sockets.adapter.rooms['room'+i]){//If room exists
+                if(io.sockets.adapter.rooms['room'+i].length < 2){//check if it waiting user
+                    socket.join('room'+i);
+                    newPlayer(socket, i);
+                    console.log(socket.id + ' has joined session ' + i);
+                    foundRoom = true;
+                    break;
+                }
             }
         }
-
+        if(!foundRoom){//If no room with waiting user
+            for(i = 0; i < roomCount; i++){
+                if(!io.sockets.adapter.rooms['room'+i]){//If room does not exist, fill in gap
+                    socket.join('room'+i);
+                    newPlayer(socket, i);
+                    console.log(socket.id + ' has joined session ' + i);
+                    break;
+                    // console.log("Room " + i + " does not exist");
+                }else if(i == roomCount - 1){
+                    console.log("New Room");
+                    socket.join('room'+roomCount);
+                    newPlayer(socket, roomCount);
+                    console.log(socket.id + ' has joined session ' + roomCount);
+                    roomCount++;
+                    break;
+                }
+            }
+        }
     }
+    // roomCount = roomCount % 128;
 
     socket.on('disconnect', function() {
         console.log('user disconnected');
