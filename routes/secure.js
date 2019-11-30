1020a54dca03c0219923bf613368829f54026eb4
FaithSN <andz.fernz@yahoo.com.ph>
2019-11-30 21:24:35 +0800

    get current user

--- routes/secure.js
+++ routes/secure.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const asyncMiddleware = require('../middleware/asyncMiddleware');
 const UserModel = require('../models/userModel')
-
+const jwt = require('jsonwebtoken')
 const router = express.Router();
  
 router.post('/submit-score', asyncMiddleware(async (req, res, next) => {
@@ -15,9 +15,16 @@ router.post('/submit-score', asyncMiddleware(async (req, res, next) => {
   res.json({ 'status': 'ok' });
   }));
 
-router.get('/scores', (req, res, next) => {
-  res.status(200);
-  res.json({ 'status': 'ok' });
-});
- 
+
+  router.get('/current', (req, res, next) => {
+    const usertoken = req.cookies['jwt'];
+    const decoded = jwt.verify(usertoken, 'top_secret');
+    return UserModel.findById(decoded.user._id)
+      .then((user) => {
+        if(!user){
+          return res.sendStatus(400)
+        }
+        return res.status(200).json(user.username)
+      });
+  });
 module.exports = router;
